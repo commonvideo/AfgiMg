11a2057383fe01e9ca35ddc41501d155ff1568b9
Common <akcommon2@gmail.com>
2024-03-04 12:36:34 +0530

    kinetic chat video call app 4.0 (Video Call Joni) -> design change of native ad

--- app/src/main/java/com/afgi/sample/MainActivity.kt
+++ app/src/main/java/com/afgi/sample/MainActivity.kt
@@ -3,22 +3,46 @@ package com.afgi.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
+import com.afgi.mg.lib.BuildConfig
 import com.afgi.mg.lib.LOADED_AD
-import com.afgi.mg.lib.requestBanner
+import com.afgi.mg.lib.initialize
 import com.afgi.mg.lib.requestLargeBanner
+import com.afgi.mg.lib.requestNative
+import com.afgi.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var binding: ActivityMainBinding
+
     var adsNativeBanner :LinearLayout ?=null
+    var adsContainer : LinearLayout ?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+/*
+        initialize()
 
         adsNativeBanner = findViewById(R.id.adsNativeBanner)
+        adsContainer = findViewById(R.id.adsContainer)
+
 
         bannerLoading(adsNativeBanner!!)
+
+        nativeAdsLoadingNative(
+            binding.cardContainer,
+            binding.adsContainer,
+            binding.adsContainerAppLovin,
+            binding.adsContainerBanner
+        )
+*/
+
     }
 
     fun bannerLoading(adsContainer: LinearLayout) {
@@ -34,4 +58,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    val listColor = intArrayOf(com.afgi.mg.lib.R.color.white, com.afgi.mg.lib.R.color.black, com.afgi.mg.lib.R.color.black, com.afgi.mg.lib.R.color.black)
+
+    fun nativeAdsLoadingNative(
+        cardView: CardView,
+        adsContainer: LinearLayout,
+        adsContainerAppLovin: LinearLayout,
+        adsContainerBanner: LinearLayout,
+
+    ) {
+                    requestNative(
+                        color = listColor,
+                        if (BuildConfig.DEBUG) "/6499/example/native" else "/6499/example/native"
+                    ) { linearLayout: LinearLayout?, status: String ->
+                        if (status == LOADED_AD) {
+                            Log.e("TAG", "native loaded: ", )
+                            adsContainer.visibility = View.VISIBLE
+                            adsContainer.removeAllViews()
+                            adsContainer.addView(linearLayout)
+                        } else {
+                            Log.e("TAG", "native failed: -----"+status )
+                        }
+                    }
+                  //  nativeCustomAds(adsContainer) }}
+
+    }
+
+
+
+
 }
